88276a56953f584598cadfa45bc728e6c37b8be0
safakhalil <saffazahid97@gmail.com>
2026-04-22 15:22:26 +0500

    Added css files

--- app.py
+++ app.py
@@ -3,18 +3,15 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
- 
 def homePage():
     return render_template("home.html")
 
-@app.route("/result" , methods=["POST","GET"])
-
+@app.route("/result", methods=["POST", "GET"])
 def resultPage():
-    name=request.form.get("name")
-    percentage=int(request.form.get("percentage"))
-    course=request.form.get(("course"))
+    name = request.form.get("name")
+    percentage = int(request.form.get("percentage"))
+    course = request.form.get("course")
     return render_template("result.html", name=name, percentage=percentage, course=course)
 
-
-if __name__ == ("__main__"):
-    app.run(debug=True,port=8080)
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
